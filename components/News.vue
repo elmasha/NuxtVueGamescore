bae74f974e687ef225af31786f20df92561cedb2
user <elmashavitran@gmail.com>
2022-03-21 19:08:07 +0300

    first commit

---
+++ components/News.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="News">
+      <p>{{ news }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "News",
+  props: ["news", "id"],
+};
+</script>
+
+<style scoped>
+.News {
+  padding: 1rem;
+  border: 1px dotted #ccc;
+  margin: 1rem 0;
+}
+</style>
